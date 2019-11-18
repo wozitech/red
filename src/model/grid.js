61179a51691c6fd8bddf86097e59d723a840f50f
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-11-18 06:43:00 +0000

    Added Grid type and Grid tests

---
+++ src/model/grid.js
@@ -0,0 +1,43 @@
+class Grid {
+  constructor() {
+    this._x = null;
+    this._y = null;
+  }
+
+  get x() {
+    return this._x;
+  }
+  set x(x) {
+    if (x<1) throw new Error('Grid x axis out of bounds');
+    this._x = x;
+    return this.x;
+  }
+
+  get y() {
+    return this._y;
+  }
+  set y(y) {
+    if (y<1) throw new Error('Grid y axis out of bounds');
+    this._y = y;
+    return this.y;
+  }
+
+  // return true if the given position (x,y) is on the grid, otherwise false
+  isOn(x,y) {
+    if (x >= 0 && x <= this._x &&
+        y >= 0 && y <= this._y) {
+      return true;
+    }
+
+    return false;
+  }
+
+  toString() {
+    return {
+      x: this._x,
+      y: this._y
+    };
+  }
+};
+
+module.exports.Grid = Grid;
--- src/model/grid.test.js
+++ src/model/grid.test.js
@@ -1,3 +1,5 @@
+const Grid = require('./grid').Grid;
+
 global.console = {
   error: jest.fn(),
   warn: jest.fn(),
@@ -9,29 +11,63 @@ describe('Grid Awareness', () => {
   beforeEach (() => {});
 
   it ('should have X and Y', () => {
+    const newGrid = new Grid();
+    newGrid.x = 4;
+    newGrid.y = 10;
+
+    expect(newGrid.x).toBe(4);
+    expect(newGrid.y).toBe(10);
   });
 
   it ('should fail if X is negative', () => {
+    const newGrid = new Grid();
+
+    expect(() => {
+      newGrid.x = 0;
+    }).toThrow(Error);
   });
 
   it ("should fail if Y is negative", () => {
+    const newGrid = new Grid();
+
+    expect(() => {
+      newGrid.y = 0;
+    }).toThrow(Error);
   });
 });
 
 describe('Grid Placement', () => {
+  let newGrid;
+  beforeAll (() => {
+    newGrid = new Grid();
+    newGrid.x = 4;
+    newGrid.y = 4;
+  });
+
   // tests the grid boundaries for a given position to whether it is on or off grid
   it('should be on grid', () => {
+    expect(newGrid.isOn(2,2)).toBe(true);
+
+    // valid boundaries
+    expect(newGrid.isOn(0,4)).toBe(true);
+    expect(newGrid.isOn(0,0)).toBe(true);
+    expect(newGrid.isOn(4,0)).toBe(true);
+    expect(newGrid.isOn(4,4)).toBe(true);
   });
 
   it('should be off top off grid', () => {
+    expect(newGrid.isOn(0,5)).toBe(false);
   });
 
   it('should be off left off grid', () => {
+    expect(newGrid.isOn(-1,4)).toBe(false);
   });
 
   it('should be off right off grid', () => {
+    expect(newGrid.isOn(5,4)).toBe(false);
   });
 
   it('should be off bottom off grid', () => {
+    expect(newGrid.isOn(4,-1)).toBe(false);
   });
 });
